818b46300987a511c8792a311e0e653a96380b26
Visforest <visforest@hotmail.com>
2024-01-08 17:25:23 +0800

    feat:add read config functions

--- kq/config.go
+++ kq/config.go
@@ -1,5 +1,14 @@
 package kq
 
+import (
+	"encoding/json"
+	"errors"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path"
+	"strings"
+)
+
 type Conf struct {
 	Brokers    []string `json:"brokers" yaml:"brokers"`       // kafka broker addresses
 	Group      string   `json:"group" yaml:"group"`           // consumer group,only for consumer
@@ -7,7 +16,35 @@ type Conf struct {
 	Processors int      `json:"processors" yaml:"processors"` // consumer count,default is the partition count of topic
 	MinBytes   int      `json:"min_bytes" yaml:"min_bytes"`   // default 10K
 	MaxBytes   int      `json:"max_bytes" yaml:"max_bytes"`   // default 10M
-	CaFile     string   `json:",optional" yaml:"ca_file"`     // certificate file path for connecting to kafka
-	Username   string   `json:",optional" yaml:"username"`    // username for connecting to kafka
-	Password   string   `json:",optional" yaml:"password"`    // password for connecting to kafka
+	CaFile     string   `json:"ca_file" yaml:"ca_file"`       // certificate file path for connecting to kafka
+	Username   string   `json:"username" yaml:"username"`     // username for connecting to kafka
+	Password   string   `json:"password" yaml:"password"`     // password for connecting to kafka
+}
+
+// LoadConfig loads Conf from specified file path
+func LoadConfig(file string) (*Conf, error) {
+	var conf Conf
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	ext := strings.ToLower(path.Ext(file))
+	switch ext {
+	case "json":
+		err = json.Unmarshal(bytes, &conf)
+	case "yaml", "yml":
+		err = yaml.Unmarshal(bytes, &conf)
+	default:
+		err = errors.New("unsupported configuration format")
+	}
+	return &conf, err
+}
+
+// MustLoadConfig loads Conf from specified file path,panics on error
+func MustLoadConfig(file string) *Conf {
+	conf, err := LoadConfig(file)
+	if err != nil {
+		panic(err)
+	}
+	return conf
 }
